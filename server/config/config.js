10d195d06f56e2eb33fe483901e597a0d0a9eb41
bbojkov <bbojkov90@googlemail.com>
2016-11-23 17:03:47 +0200

    Hopefully last updates

--- server/config/config.js
+++ server/config/config.js
@@ -2,18 +2,13 @@
 
 const path = require("path");
 const getRootPath = path.normalize(path.join(__dirname, "/../../"));
-
+const localHostConnectionString = "mongodb://localhost:27017/infshroom";
 
 module.exports = {
-    // development: {
-    //     rootPath: getRootPath,
-    //     db: "mongodb://localhost:27017/shroomportal",
-    //     port: 1337
-    // },
     development: {
         rootPath: getRootPath,
-        db: "mongodb://commodore:pass1@ds159377.mlab.com:59377/infected_mushroom",
-        port: 59377
+        db: process.env.MONGOLAB_URI || localHostConnectionString, //"mongodb://commodore:pass1@ds159377.mlab.com:59377/infected_mushroom",
+        port: Number(process.env.PORT || 3000)
     }
     // production: {
     //     rootPath: getRootPath,
--- server/config/express.js
+++ server/config/express.js
@@ -4,9 +4,9 @@ const express = require("express");
 
 module.exports = (config, app) => {
     app.set("view engine", "pug");
-    app.set("views", `${config.rootPath}server\\views`);
+    app.set("views", `${config.rootPath}server/views`);
 
-    app.use(express.static(`${config.rootPath}\\public`));
+    app.use(express.static(`${config.rootPath}/public`));
 
     console.log(config.rootPath);
 };
