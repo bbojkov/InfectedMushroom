5ba4a9890a1d88810430bd414f0b91897eaae6a3
bbojkov <bbojkov90@googlemail.com>
2016-11-28 14:14:54 +0200

    Fixed bugs with register and typos

--- server/controllers/users-controller.js
+++ server/controllers/users-controller.js
@@ -1,51 +1,50 @@
-module.exports = function(data) {
+module.exports = function (data) {
     return {
         register: (req, res) => {
             let user = req.body;
             if (user.password !== user.confirmPassword) {
-
                 res.render('../views/portal.pug', { globalError: 'Pass not matching' });
-
             } else {
                 //encrypt the pass
                 //other user like this ? with the same mail ? with the same username
                 data.users.createUser(user)
-                then(user => {
-                    req.logIn(user, (err, user) => {
-                        if (err) {
-                            console.log('Cant create user!!!');
-                            return
-                        }
-                        res.redirect("/");
+                    .then(returnUser => {
+                        req.logIn(returnUser, (err, loggedUser) => {
+                            if (err) {
+                                console.log('Cant create user!!!');
+                                return err;
+                            }
+                            res.redirect("/");
+                        });
                     });
-                });
             }
         },
         login: (req, res) => {
             let usersCredentials = req.body;
             data.users.findByUsername(usersCredentials.username)
-                .then(user => {
-                    if (!user) {
+                .then(foundUser => {
+                    if (!foundUser) {
                         console.log("Invalid username and pass");
                         return;
                     }
-                    if (user.authenticate(usersCredentials.password)) {
-                        req.logIn(user, (err, user) => {
+                    if (foundUser.authenticate(usersCredentials.password)) {
+                        req.logIn(foundUser, (err, loggedUser) => {
                             if (err) {
                                 console.log('Cant login user!!!');
-                                return
+                                return err;
                             }
                             res.redirect("/");
                         });
                     }
-                    else{
-                        console.log('Treshti qko pls')
+                    else {
+                        console.log("Treshti qko pls");
+                        return new Error("treshti");
                     }
                 });
         },
         logout: (req, res) => {
             res.logout();
-            res.redirect("/")
-        },
-    }
+            res.redirect("/");
+        }
+    };
 };
