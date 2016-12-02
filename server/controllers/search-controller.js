9713133bbf44a00aca519322ad222e31f0d1d9d5
Brevering <stoevs@gmail.com>
2016-12-02 17:07:29 +0200

    Search finished (for now... )

--- server/controllers/search-controller.js
+++ server/controllers/search-controller.js
@@ -1,13 +1,13 @@
 "use strict";
 
 const DEFAULT_PAGE = 1,
-    PAGE_SIZE = 2;
+    PAGE_SIZE = 5;
 
 module.exports = function(data) {
     return {
         search: (req, res) => {
-            let pattern = req.query.pattern || "";
-            let page = Number(req.query.page || DEFAULT_PAGE);
+            let pattern = req.query.val || "";
+            let page = Number(req.body.page || DEFAULT_PAGE);
 
             return Promise.all([data.news.searchNews({ pattern, count: PAGE_SIZE, page }), data.guides.searchGuides({ pattern, count: PAGE_SIZE, page })])
                 .then(([news, guides]) => {
